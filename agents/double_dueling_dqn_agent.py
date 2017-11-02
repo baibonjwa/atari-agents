fe4bf5e726567dfc7618b62ef3f2f4e4b6bb869e
happybai <byron.bai@aol.com>
2017-11-02 12:28:38 +0800

    Fix params

--- agents/double_dueling_dqn_agent.py
+++ agents/double_dueling_dqn_agent.py
@@ -13,47 +13,49 @@ from .utils import rgb2gray
 class Qnetwork():
     def __init__(self, h_size, action_space):
         self.scalarInput = tf.placeholder(shape=[None, 33600], dtype=tf.float32)
-        self.imageIn = tf.reshape(self.scalarInput, shape=[-1, 210, 160])
+        self.imageIn = tf.reshape(self.scalarInput, shape=[-1, 210, 160, 1])
         self.conv1 = slim.conv2d(
             inputs=self.imageIn,
             num_outputs=32,
-            kernel_size=8,
+            kernel_size=4,
             stride=7,
             padding='VALID',
             biases_initializer=None
         )
+
         self.conv2 = slim.conv2d(
             inputs=self.conv1,
             num_outputs=64,
-            kernel_size=4,
+            kernel_size=2,
             stride=3,
             padding='VALID',
             biases_initializer=None
         )
+
         self.conv3 = slim.conv2d(
             inputs=self.conv2,
             num_outputs=64,
-            kernel_size=3,
+            kernel_size=2,
             stride=2,
             padding='VALID',
             biases_initializer=None
         )
 
-        #  self.conv4 = slim.conv2d(
-            #  inputs=self.conv3,
-            #  num_outputs=64,
-            #  kernel_size=7,
-            #  stride=1,
-            #  padding='VALID',
-            #  biases_initializer=None
-        #  )
+        self.conv4 = slim.conv2d(
+            inputs=self.conv3,
+            num_outputs=64,
+            kernel_size=4,
+            stride=2,
+            padding='VALID',
+            biases_initializer=None
+        )
+        self.fc = slim.fully_connected(
+            self.conv4,
+            num_outputs=h_size,
+            activation_fn=tf.tanh,
+        )
 
-
-        #  self.fc = slim.fully_connected(
-            #  self.conv3,
-            #  num_outputs=23,
-            #  activation_fn=tf.tanh,
-        #  )
+        #  pdb.set_trace()
 
         #  self.conv4 = slim.conv2d(
             #  inputs=self.conv3,
@@ -67,7 +69,7 @@ class Qnetwork():
         # Dueling network
         #  self.streamAC, self.streamVC = tf.split(self.conv4, 2, 3)
         #  pdb.set_trace()
-        self.streamAC, self.streamVC = tf.split(self.conv3, 2)
+        self.streamAC, self.streamVC = tf.split(self.fc, 2, 3)
         self.streamA = slim.flatten(self.streamAC)
         self.streamV = slim.flatten(self.streamVC)
         xavier_init = tf.contrib.layers.xavier_initializer()
@@ -75,9 +77,7 @@ class Qnetwork():
         self.VW = tf.Variable(xavier_init([h_size//2, 1]))
         self.Advantage = tf.matmul(self.streamA, self.AW)
         self.Value = tf.matmul(self.streamV, self.AW)
-
         # ?
-        pdb.set_trace()
         self.Qout = self.Value + tf.subtract(self.Advantage, tf.reduce_mean(self.Advantage, axis=1, keep_dims=True))
         self.predict = tf.argmax(self.Qout, 1)
 
@@ -91,6 +91,24 @@ class Qnetwork():
         self.loss = tf.reduce_mean(self.td_error)
         self.trainer = tf.train.AdamOptimizer(learning_rate=0.0001)
         self.updateModel = self.trainer.minimize(self.loss)
+
+        print("scalarInput: %s" % self.scalarInput.shape)
+        print("imageIn: %s" % self.imageIn.shape)
+        print("conv1: %s" % self.conv1.shape)
+        print("conv2: %s" % self.conv2.shape)
+        print("conv3: %s" % self.conv3.shape)
+        print("conv4: %s" % self.conv4.shape)
+        print("fc: %s" % self.fc.shape)
+        print("streamAC: %s" % self.streamAC.shape)
+        print("streamVC: %s" % self.streamVC.shape)
+        print("streamA: %s" % self.streamA.shape)
+        print("streamV: %s" % self.streamV.shape)
+        print("AW: %s" % self.AW.shape)
+        print("VW: %s" % self.VW.shape)
+        print("Advantage: %s" % self.Advantage.shape)
+        print("Value: %s" % self.Value.shape)
+        print("Qout: %s" % self.Qout.shape)
+        print("predict: %s" % self.predict.shape)
 
 class experience_buffer():
     def __init__(self, buffer_size=50000):
@@ -113,6 +131,7 @@ class DoubleDuelingDQNAgent(object):
         self.action_space = env.action_space
 
         self.config = {
+            #  "batch_size": 16,
             "batch_size": 32,
             #  "batch_size": 8,
             "update_freq": 4,
@@ -126,6 +145,7 @@ class DoubleDuelingDQNAgent(object):
             "load_model": False,
             "path": "./dqn",
             "h_size": 512,
+            #  "h_size": 1024,
             "tau": 0.001,
         }
 
@@ -174,12 +194,28 @@ class DoubleDuelingDQNAgent(object):
 
             if self.total_steps % (self.config["update_freq"]) == 0:
                 trainBatch = myBuffer.sample(self.config["batch_size"])
+                scalarInput, imageIn, Q, val, adv, AC, VC, A, V, AW, VW, conv1, conv2, conv3, fc = self.sess.run([
+                    self.mainQN.scalarInput,
+                    self.mainQN.imageIn,
+                    self.mainQN.predict,
+                    self.mainQN.Value,
+                    self.mainQN.Advantage,
+                    self.mainQN.streamAC,
+                    self.mainQN.streamVC,
+                    self.mainQN.streamA,
+                    self.mainQN.streamV,
+                    self.mainQN.AW,
+                    self.mainQN.VW,
+                    self.mainQN.conv1,
+                    self.mainQN.conv2,
+                    self.mainQN.conv3,
+                    self.mainQN.fc,
+                ], feed_dict={self.mainQN.scalarInput:np.vstack(trainBatch[:, 3])})
                 Q1 = self.sess.run(self.mainQN.predict, feed_dict={self.mainQN.scalarInput:np.vstack(trainBatch[:, 3])})
                 Q2 = self.sess.run(self.targetQN.Qout, feed_dict={self.targetQN.scalarInput:np.vstack(trainBatch[:, 3])})
                 end_multiplier = -(trainBatch[:, 4] - 1)
                 doubleQ = Q2[range(self.config["batch_size"]), Q1]
                 targetQ = trainBatch[:, 2] + (self.config["y"] * doubleQ * end_multiplier)
-                #  pdb.set_trace()
                 _, loss = self.sess.run([self.mainQN.updateModel, self.mainQN.loss], feed_dict={ self.mainQN.scalarInput:np.vstack(trainBatch[:, 0]), self.mainQN.targetQ:targetQ, self.mainQN.actions:trainBatch[:, 1] })
                 self.loss = loss
                 self.updateTarget(self.targetOps, self.sess)
